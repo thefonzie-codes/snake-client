ea44448893c6f3876f04206f7d15168f2f853dc9
Alfonso <alfonsobanzon@gmail.com>
2023-09-27 16:57:57 +0000

    client.js created, now contains connection info.

---
+++ client.js
@@ -0,0 +1,17 @@
+const connect = () => {
+  const conn = net.createConnection(
+  {
+    host:"165.227.47.243 ",
+    port:"50541"
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data)
+  });
+
+return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,6 @@
 const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection(
-  {
-    host:"165.227.47.243 ",
-    port:"50541"
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log(data)
-  });
-
-return conn;
-};
+const connect = require('./client');
 
 console.log('connecting ...');
 connect();
+
