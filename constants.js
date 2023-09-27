5077530eff9e62388cc017bbc537813c1ee991bd
Alfonso <alfonsobanzon@gmail.com>
2023-09-27 19:51:28 +0000

    inputs an object in constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,24 @@
 const IP = "165.227.47.243";
 const PORT = "50541";
 
+const inputs = {
+    
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+
+  //messages
+  
+  q: "Say: Hiiiiii!",
+  e: "Say: EZ",
+  z: "Say: POG",
+  x: "Say: Boop",
+  c: "Say: Hiiiiii!",
+}
+
 module.exports = {
   IP,
-  PORT
+  PORT,
+  inputs
 };
--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const constants = require('./constants');
+
 let connection;
 
 const setupInput = (conn) => {
@@ -13,43 +15,47 @@ const setupInput = (conn) => {
 };
 
 const handleUserInput = (conn) => {
-
-  if (conn === 'w') {
-  connection.write("Move: up")
-  }
-
-  if (conn === 'a') {
-  connection.write("Move: left")
-  }
-
-  if (conn === 's') {
-  connection.write("Move: down")
-  }
-
-  if (conn === 'd') {
-  connection.write("Move: right")
-  }
-
-  if (conn === 'q') {
-  connection.write("Say: Hiiiiii!")
-  }
-
-  if (conn === 'e') {
-  connection.write("Say: EZ")
-  }
-
-  if (conn === 'x') {
-  connection.write("Say: POG")
-  }
-
-  if (conn === 'x') {
-  connection.write("Say: Boop")
-  }
-
+  
   if (conn === '\u0003') {
-  console.log("exiting")
-  process.exit();
+    console.log("exiting")
+    process.exit();
   }
+
+  connection.write(constants.inputs[conn]);
 };
+  // if (conn === 'w') {
+  // connection.write("Move: up")
+  // }
+
+  // if (conn === 'a') {
+  // connection.write("Move: left")
+  // }
+
+  // if (conn === 's') {
+  // connection.write("Move: down")
+  // }
+
+  // if (conn === 'd') {
+  // connection.write("Move: right")
+  // }
+
+  // if (conn === 'q') {
+  // connection.write("Say: Hiiiiii!")
+  // }
+
+  // if (conn === 'e') {
+  // connection.write("Say: EZ")
+  // }
+
+  // if (conn === 'x') {
+  // connection.write("Say: POG")
+  // }
+
+  // if (conn === 'x') {
+  // connection.write("Say: Boop")
+  // }
+
+
+//};
 
 module.exports = setupInput;
